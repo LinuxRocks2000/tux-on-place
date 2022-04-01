09da5b6d97ea6429c1071b4651e028d7490bd5c9
The Buzzard <plupy@awesome.local>
2022-04-01 15:35:44 -0400

    Make it work for Israel

--- gen.py
+++ gen.py
@@ -1,19 +1,20 @@
 #!/bin/env python3
 import matplotlib.pyplot as plt
+import math
 
-p = plt.imread("tux.png")
+p = plt.imread("israel.png")
 
-topleft = (20, 679)
+topleft = (20, 643)
 
 scale = 10
 
 generated = ""
 
-for row in range(len(p)):
-    generated += f"<div class='row' style='top: {row * scale}px;'>"
-    for square in range(len(p[row])):
-        color = f"rgba({p[row][square][0]*255},{p[row][square][1]*255},{p[row][square][2]*255},{p[row][square][3]})"
-        generated += f"<a style='background-color: {color};left: {square * scale}px;' class='square' href='https://new.reddit.com/r/place/?cx={topleft[0] + square}&cy={topleft[1] + row}&px=20'></a>\n"
+for row in range(0, len(p), 8):
+    generated += f"<div class='row' style='top: {row/8 * scale}px;'>"
+    for square in range(0, len(p[row]), 8):
+        color = f"rgb({p[row][square][0]*255},{p[row][square][1]*255},{p[row][square][2]*255})"
+        generated += f"<a style='background-color: {color};left: {square/8 * scale}px;' class='square' href='https://new.reddit.com/r/place/?cx={topleft[0] + square}&cy={topleft[1] + row}&px=20'></a>\n"
     generated += "</div>"
 
 template = f"""
